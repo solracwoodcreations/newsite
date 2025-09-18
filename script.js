f1180b2cd003b2228fe089c049d3b4aaf0ec5ffc
solrac <119016954+solracwoodcreations@users.noreply.github.com>
2025-09-17 20:04:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-hello
+function showSidebar(){
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'flex'
+        }
+        function hideSidebar(){
+            const sidebar = document.querySelector('.sidebar')
+            sidebar.style.display = 'none'
+        }
